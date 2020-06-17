410a21a99bf19ad055bc56b891e1ba02c4bca9da
Daniel Sprague <danielaaronsprague@gmail.com>
2020-06-17 15:15:04 -0500

    react95 bootstrap

--- client/app.js
+++ client/app.js
@@ -2,12 +2,21 @@ import React from 'react'
 
 import {Navbar} from './components'
 import Routes from './routes'
+import {createGlobalStyle, ThemeProvider} from 'styled-components'
+import {reset, themes, List, ListItem, Divider} from 'react95'
+
+const ResetStyles = createGlobalStyle`
+  ${reset}
+`
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Routes />
+      <ResetStyles />
+      <ThemeProvider theme={themes.default}>
+        <Navbar />
+        <Routes />
+      </ThemeProvider>
     </div>
   )
 }
