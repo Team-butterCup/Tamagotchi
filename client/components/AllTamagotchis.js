da1303eb46eb62ab796e4b210a6145b8239bee96
Daniel Sprague <danielaaronsprague@gmail.com>
2020-06-17 17:29:11 -0500

    AllTamagotchis component has basic functionality

---
+++ client/components/AllTamagotchis.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {NavLink} from 'react-router-dom'
+//import AddTamagotchiForm from "./AddTamagotchiForm";
+//import DeleteButton from "./DeleteButton";
+
+export const AllTamagotchis = props => {
+  const tamagotchis = props.tamagotchis
+  console.log(tamagotchis)
+  return (
+    <div>
+      <div>
+        <h1>Browse Adoptable Tamagotchis</h1>
+      </div>
+      {/* <div>
+        <AddTamagotchiForm />
+      </div> */}
+      {tamagotchis !== undefined && tamagotchis.length ? (
+        tamagotchis.map(tamagotchi => (
+          <div key={tamagotchi.id}>
+            <NavLink to={`/Tamagotchis/${tamagotchi.id}`}>
+              <div className="Tamagotchis">
+                <div>
+                  <img src={tamagotchi.imageUrl} width="100"></img>
+                </div>
+                <div>{tamagotchi.name}</div>
+              </div>
+            </NavLink>
+            {/* <DeleteButton TamagotchiId={Tamagotchi.id} name={Tamagotchi.name} /> */}
+          </div>
+        ))
+      ) : (
+        <h2>Loading</h2>
+      )}
+    </div>
+  )
+}
+
+const mapStateToProps = reduxState => {
+  return {
+    tamagotchis: reduxState.tamagotchis
+  }
+}
+
+export default connect(mapStateToProps)(AllTamagotchis)
---
+++ client/components/SingleTamagotchi.js
@@ -0,0 +1,7 @@
+import React from 'react'
+//import {fetchSingleTamagotchi} from '../store/singleTamagotchi'
+import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom'
+//import Reviews from './review'
+
+export default null
